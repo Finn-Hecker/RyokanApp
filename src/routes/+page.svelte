209eda85b83c7da0b549a2c98ce6d6b45af5d049
Finn-Hecker <Finn.Hecker3@gmail.com>
2026-02-20 16:35:12 +0100

    chore: rename ryokan-io to ryokan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,12 +29,6 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
       </button>
       
-      {#if $currentView === 'lobby'}
-        <span class="text-xs font-medium tracking-[0.2em] text-gray-600 uppercase pointer-events-auto">
-          {m.app_name()}
-        </span>
-      {/if}
-      
       <div class="w-10"></div> 
     </header>
   {/if}
