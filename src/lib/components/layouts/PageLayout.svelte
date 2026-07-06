eb225db011b78e0858b535563b1ac53d51acf5c9
Finn-Hecker <Finn.Hecker3@gmail.com>
2026-07-06 19:50:56 +0200

    style(layout): hide content scrollbar

--- src/lib/components/layouts/PageLayout.svelte
+++ src/lib/components/layouts/PageLayout.svelte
@@ -55,7 +55,7 @@
     {/if}
   {/if}
 
-  <div class="flex-1 overflow-y-auto min-w-0">
+  <div class="flex-1 overflow-y-auto min-w-0 scrollbar-hide">
     <div class="{maxContentWidth} mx-auto w-full {contentPadding}">
       
       <div class="flex items-center justify-between pt-[calc(1rem+env(safe-area-inset-top))] mb-6 md:pt-6">
@@ -85,3 +85,12 @@
     </div>
   </div>
 </div>
+<style>
+  .scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+.scrollbar-hide {
+  -ms-overflow-style: none; /* IE/Edge */
+  scrollbar-width: none;    /* Firefox */
+}
+</style>
